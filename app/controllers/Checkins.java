24792c51f9808f7d06f1e174bdf0991e3adf8ab7
JeffYin <zhenlingyin@gmail.com>
2012-06-26 22:02:26 -0400

    Added the Checkin page(not finished)

---
+++ app/controllers/Checkins.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import java.lang.reflect.Constructor;
+import java.util.List;
+
+import models.Item;
+
+import play.db.Model;
+import play.exceptions.TemplateNotFoundException;
+import controllers.CRUD.ObjectType;
+import flexjson.JSONSerializer;
+
+public class Checkins extends CRUD{
+	public static void blank() throws Exception {
+            render("Checkins/scanItem.html");
+	}
+	
+	public static void checkin(String[] itemBarcodeScanned) throws Exception {
+		
+	}
+	
+	public static void scanItem(String barcode) throws Exception {
+		// 
+		//TODO: Lookup the barcode from the borrowed material.
+		List<Item> items = Item.find("barcode = ?", barcode).fetch();
+		int numberFound = items.size();
+		if (numberFound==1) { /* found the exact item */
+			Item item = items.get(0);
+			String info = toJson(item);
+			renderJSON(info);
+		} else if (numberFound==0) { /* found nothing */
+			response.status =  700;
+		} else { /* more than one items found */
+            response.status = 710;			
+		}
+	}
+	
+	private static String toJson(Item item) {
+		JSONSerializer borrowedItemSerializer = new JSONSerializer().include("barcode", "name").exclude("*");
+		String json = borrowedItemSerializer.serialize(item);
+		return json;
+	}
+}
---
+++ app/models/Checkin.java
@@ -0,0 +1,15 @@
+package models;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+/**
+ * This Entity does nothing just for displaying the Checkin item on the home page. 
+ * @author Developer
+ *
+ */
+@Entity
+public class Checkin extends Model {
+
+}
